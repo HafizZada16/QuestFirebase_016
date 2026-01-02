42ae0a32aeec6c1c106f3cf79d812958efe58375
HafizZada16 <hafizzada16@gmail.com>
2026-01-02 18:13:38 +0700

    Add code DestinasiHome.kt

--- app/src/main/java/com/example/questfirebase_016/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/questfirebase_016/view/route/DestinasiHome.kt
@@ -1,4 +1,8 @@
 package com.example.questfirebase_016.view.route
 
-object DestinasiHome {
+import com.example.questfirebase_016.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
 }
