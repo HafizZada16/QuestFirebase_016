4c2d1b4a6fb5f5d7e31e4a09137ea97ed0abfb69
HafizZada16 <hafizzada16@gmail.com>
2026-01-02 18:44:55 +0700

    Add code PetaNavigasi.kt

--- app/src/main/java/com/example/questfirebase_016/view/controlNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/questfirebase_016/view/controlNavigasi/PetaNavigasi.kt
@@ -1,2 +1,36 @@
 package com.example.questfirebase_016.view.controlNavigasi
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questfirebase_016.view.EntrySiswaScreen
+import com.example.questfirebase_016.view.HomeScreen
+import com.example.questfirebase_016.view.route.DestinasiDetail
+import com.example.questfirebase_016.view.route.DestinasiEntry
+import com.example.questfirebase_016.view.route.DestinasiHome
+
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier = Modifier) {
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier) {
+        composable(DestinasiHome.route) {
+            HomeScreen(navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                onDetailClick = { navController.navigate("${DestinasiDetail.route}/${it}") })
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) })
+        }
+    }
+}
+
