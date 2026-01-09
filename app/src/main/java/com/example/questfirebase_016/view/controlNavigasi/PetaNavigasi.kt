009dbfbed7b70b32392ac26b886df3eecb62203f
HafizZada16 <hafizzada16@gmail.com>
2026-01-09 17:02:07 +0700

    Add code Final

--- app/src/main/java/com/example/questfirebase_016/view/controlNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/questfirebase_016/view/controlNavigasi/PetaNavigasi.kt
@@ -8,6 +8,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.questfirebase_016.view.DetailSiswaScreen
 import com.example.questfirebase_016.view.EditSiswaScreen
 import com.example.questfirebase_016.view.EntrySiswaScreen
 import com.example.questfirebase_016.view.HomeScreen
