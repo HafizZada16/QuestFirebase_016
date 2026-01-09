5e919d618a2b0a5ad817c48d38c3c3d7d75fff3e
HafizZada16 <hafizzada16@gmail.com>
2026-01-09 15:49:18 +0700

    Add code Form Input dan Logika Simpan Data ke Database

--- app/src/main/java/com/example/questfirebase_016/view/HalamanEdit.kt
+++ app/src/main/java/com/example/questfirebase_016/view/HalamanEdit.kt
@@ -1,14 +1,17 @@
 package com.example.questfirebase_016.view
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.questfirebase_016.view.route.DestinasiEdit
 import com.example.questfirebase_016.viewmodel.EditViewModel
 import com.example.questfirebase_016.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -28,6 +31,17 @@ fun EditSiswaScreen(
         },
         modifier = modifier
     ) { innerPadding ->
-
+        val coroutineScope = rememberCoroutineScope()
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.editSatuSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
     }
 }
