fe40272e0d4fbe71914c112f54e482248f101002
HafizZada16 <hafizzada16@gmail.com>
2026-01-09 15:39:19 +0700

    Add code ambil data siswa

--- app/src/main/java/com/example/questfirebase_016/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questfirebase_016/viewmodel/DetailViewModel.kt
@@ -6,10 +6,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.questfirebase_016.modeldata.Siswa
 import com.example.questfirebase_016.repositori.RepositorySiswa
 import com.example.questfirebase_016.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
 import kotlinx.serialization.InternalSerializationApi
+import java.io.IOException
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: Siswa?) : StatusUIDetail
@@ -23,9 +26,25 @@ RepositorySiswa): ViewModel() {
     private val idSiswa: Long =
         savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
             ?: error("idSiswa tidak ditemukan di SavedStateHandle")
-    var statusUIDetail:StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
         private set
 
     init {
         getSatuSiswa()
     }
+
+    fun getSatuSiswa(){
+        viewModelScope.launch {
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail = try {
+                StatusUIDetail.Success(satusiswa = repositorySiswa.getSatuSiswa(idSiswa) )
+            }
+            catch (e: IOException){
+                StatusUIDetail.Error
+            }
+            catch (e: Exception){
+                StatusUIDetail.Error
+            }
+        }
+    }
+}
