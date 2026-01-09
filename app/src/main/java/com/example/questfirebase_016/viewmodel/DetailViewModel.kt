d6f0cf50301f3dfdc4ef7a01f07bcbdff629a924
HafizZada16 <hafizzada16@gmail.com>
2026-01-09 15:35:12 +0700

    Add code Sealed Interface UIState

--- app/src/main/java/com/example/questfirebase_016/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questfirebase_016/viewmodel/DetailViewModel.kt
@@ -1,2 +1,9 @@
 package com.example.questfirebase_016.viewmodel
 
+import com.example.questfirebase_016.modeldata.Siswa
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
